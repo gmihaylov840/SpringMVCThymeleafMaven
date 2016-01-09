d7dc5d358302ffbe3d8c972dc40867de491f820a
extendet <georgi.mm@gmail.com>
2016-01-09 18:48:40 +0200

    Project content

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SpringMVCThymeleafMaven
+Demo setup for Spring MVC (4.2.2.RELEASE) with Thymeleaf (3.0.0.BETA01) managed with Maven
+
+To run in embedded Jetty server simply issue the following command to Maven "jetty:run".
+
+Index.html is accessible on "http://localhost:8080/myApp/".
---
+++ src/main/java/thymeleaf/mvc/setup/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package thymeleaf.mvc.setup;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "thymeleaf.setup", "thymeleaf.mvc.controller" })
+public class ApplicationConfiguration {
+
+}
